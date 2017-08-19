54fe0e7cdd88a822d0e1fb556c2aa8263a7a2f9a
GitYourselfGudBucko <30606343+GitYourselfGudBucko@users.noreply.github.com>
2017-08-19 17:21:29 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
 	$('#top').css('display', 'none');
 	$('#top').fadeIn(2000);
-	$('#side').css('display', 'none');
-	$('#side').fadeIn(2000);
+	$('#side-image').css('display', 'none');
+	$('#side-image').fadeIn(2000);
+	$('.profile-box').css('display', 'none');
+	$('.profile-box').fadeIn(2000);
+
+	$('.profile-box').click(function() {
+  	$(this).animate({
+    	left: "+=15",
+    	}, 5000, function() {
+    	
+  });
+});
 	
-	$('profile-box').click(function() {
-		$(this).animate({left: '250px'});
-	});
 });
