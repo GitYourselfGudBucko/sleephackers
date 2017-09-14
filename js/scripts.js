3f059e2546eece935a6c5f0f76eba602f2d1970a
Alanocallaghan <alan.ocallaghan@outlook.com>
2017-09-14 10:22:18 +0100

    Add volume/issue functionality, don't make img tag in jq (#3)
    * Add volume/issue functionality, don't make img tag in jq
    
    * INDENT WITH SPACES YOU FUGGING PLEB

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,135 @@
 $(document).ready(function() {
-	$('#top').css('display', 'none');
-	$('#top').fadeIn(2000);
-	$('#side-image').css('display', 'none');
-	$('#side-image').fadeIn(2000);
-	$('.profile-box').css('display', 'none');
-	$('.profile-box').fadeIn(2000);
-
-// set up an array of comic images
-var imgs = [
-	  'images/Page-01.jpg',
-   	'images/Page-02.jpg',
-    'images/Page-03.jpg',
-    'images/Page-04.jpg'
- ],
-    // initialize current to 0
-    current = 0;
+    $('#top').css('display', 'none');
+    $('#top').fadeIn(2000);
+    $('#side-image').css('display', 'none');
+    $('#side-image').fadeIn(2000);
+    $('.profile-box').css('display', 'none');
+    $('.profile-box').fadeIn(2000);
 
 
-// define a function that returns the next item in the imgs array,
-// or the first if we're already at the last one
-function next() {
-    current ++;
-    if (current == imgs.length) current = 0;
-    updateImage(current);
-}
-
-// define a function to do the opposite of next()
-function prev() {
-    current --;
-    if (current < 0) current = imgs.length - 1;
-    updateImage(current);
-}
-
-// my alpha and omega
-function alpha() {
-    current = 0;
-	  updateImage(current);
-}
-
-function omega() {
-    current = imgs.length - 1;
-	  updateImage(current);
-}
+    // set up an array of comic images
+    var imgs = [
+        'images/volumes/vol-01/issue-01/page-01.jpg',
+        'images/volumes/vol-01/issue-01/page-02.jpg',
+        'images/volumes/vol-01/issue-01/page-03.jpg',
+        'images/volumes/vol-01/issue-01/page-04.jpg'
+     ],
+        // initialize current_page to 0
+        current_image = 0,
+        current_page = 0,
+        current_issue = 0,
+        current_volume = 0,
+        volume_names = ["grounding"],
+        issue_names = [
+            ["enter the metropolis", "exit the metropolis"],
+        ];
 
 
-function updateImage(index) {
-    if (current == 0) {
-        $(".prev").hide();
-    } else {
-        $(".prev").show();
+    function countPagesInIssue(volume, issue) {
+        var pages = 0;
+        var volume_regex = new RegExp('vol-' + ("00" + volume).slice(-2));
+        var issue_regex = new RegExp('issue-' + ("00" + issue).slice(-2));
+        for (var i = 0; i < imgs.length; i ++) {
+            if (imgs[i].match(volume_regex) && imgs[i].match(issue_regex)) {
+                pages ++;
+            }
+        }
+        return pages;
     }
 
-    if (current == imgs.length - 1) {
-        $(".next").hide();
-    } else {
-        $(".next").show();
+    console.log(countPagesInIssue(1, 1));
+
+    function parseImageURL(url) {
+        var regex = /vol-(\d+)\/issue-(\d+)\/page-(\d+)/;
+        url.replace(regex, function(match, p1, p2, p3) {
+            current_volume = parseInt(p1) - 1;
+            $("#volume_number").html("v." + p2 + ": " + volume_names[current_volume]);
+            current_issue = parseInt(p2) - 1;
+            $("#issue_number").html("i." + p2 + ": " + issue_names[current_volume][current_issue]);
+            current_page = parseInt(p3) - 1;
+            $("#page_number").html("p." + p3);
+        });
     }
 
-    comic.attr('src', imgs[index]);
-}
+    // define a function that returns the next item in the imgs array,
+    // or the first if we're already at the last one
+    function next() {
+        current_image ++;
+        if (current_image == imgs.length) current_image = 0;
+        updateImage(current_image);
+    }
+
+    // define a function to do the opposite of next()
+    function prev() {
+        current_image --;
+        if (current_image < 0) current_image = imgs.length - 1;
+        updateImage(current_image);
+    }
+
+    // my alpha and omega
+    function alpha() {
+        current_image = 0;
+        updateImage(current_image);
+    }
+
+    function omega() {
+        current_image = imgs.length - 1;
+        updateImage(current_image);
+    }
+
+    function updateImage(index) {
+        if (current_image == 0) {
+            $(".prev").hide();
+        } else {
+            $(".prev").show();
+        }
+
+        if (current_image == imgs.length - 1) {
+            $(".next").hide();
+        } else {
+            $(".next").show();
+        }
+        var image = imgs[index];
+        parseImageURL(image);
+        comic.attr('src', image);
+    }
+
+    var comic = $("#comic");
+    // append to DOM
+    updateImage(0);
+
+    // click the prev button, get the previous image
+    $('.prev').on('click', prev);
+    // click the next button, get the next image
+    $('.next').on('click', next);
+    $('.alpha').on('click', alpha);
+    $('.omega').on('click', omega);
+    $('#page').on('click', next);
 
 
-// define the first image in terms of a jquery object
-var comic = $('<img/>');
-// append to DOM
-$('#page').append(comic);
-updateImage(0);
-// click the prev button, get the previous image
-$('.prev').on('click', prev);
-// click the next button, get the next image
-$('.next').on('click', next);
-$('.alpha').on('click', alpha);
-$('.omega').on('click', omega);
-$('#page').on('click', next);
 
 
 
+    // scrolling animations
 
 
-// scrolling animations
+    // $('#nav-bar').click(
+    //     function() {
+    //         $(this).animate({
+    //             top: "+=200",
+    //         },
+    //         5000,
+    //         function() {
+    //             // Animation complete.
+    //         });
+    //     });
+    // });
 
 
-// $('#nav-bar').click(
-//     function() {
-//         $(this).animate({
-//             top: "+=200",
-//         },
-//         5000,
-//         function() {
-//             // Animation complete.
-//         });
-//     });
-// });
-
-
-// $(window).scroll(function() {
-//     console.log($(window).scrollTop());
-// 	   $("#nav-bar").stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, "slow" );
-//     });
+    // $(window).scroll(function() {
+    //     console.log($(window).scrollTop());
+    //     $("#nav-bar").stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, "slow" );
+    //     });
 
 
 
